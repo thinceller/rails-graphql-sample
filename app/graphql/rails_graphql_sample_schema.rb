d67125f0542b2b03bb89856c037bfa5d3651a9eb
thinceller <kohei.ct@icloud.com>
2020-02-02 05:18:07 +0900

    Execute rails g graphql:install

---
+++ app/graphql/rails_graphql_sample_schema.rb
@@ -0,0 +1,10 @@
+class RailsGraphqlSampleSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+
+  # Opt in to the new runtime (default in future graphql-ruby versions)
+  use GraphQL::Execution::Interpreter
+
+  # Add built-in connections for pagination
+  use GraphQL::Pagination::Connections
+end
