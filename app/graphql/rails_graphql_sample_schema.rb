a9a09765ddaa62a198a1db786bc121a744589084
thinceller <kohei.ct@icloud.com>
2020-02-02 17:45:49 +0900

    Fix error

--- app/graphql/rails_graphql_sample_schema.rb
+++ app/graphql/rails_graphql_sample_schema.rb
@@ -4,6 +4,7 @@ class RailsGraphqlSampleSchema < GraphQL::Schema
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
